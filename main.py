925692c60770f027b05e1f0c857755fa26de4173
Instituto Ruy Palhano <ti@ruypalhano.com.br>
2025-03-01 19:32:19 -0300

    Mensagem do commit

--- main.py
+++ main.py
@@ -1,47 +1,36 @@
+import pymssql
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
-import pyodbc
-from dotenv import load_dotenv
 import os
-
-# Carregar variáveis de ambiente do arquivo .env
-load_dotenv()
 
 app = FastAPI()
 
-# Configurações de conexão com o SQL Server (lidas do .env)
+# Configurações de conexão com o SQL Server
 SERVER = os.getenv("DB_SERVER")
 DATABASE = os.getenv("DB_DATABASE")
 USERNAME = os.getenv("DB_USERNAME")
 PASSWORD = os.getenv("DB_PASSWORD")
-DRIVER = os.getenv("DB_DRIVER")
-
-# String de conexão
-CONNECTION_STRING = f'DRIVER={DRIVER};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
 
 
 @app.get("/vwcliente/")
 async def get_vwcliente():
-    conn = None
     try:
-        # Conectar ao banco de dados
-        conn = pyodbc.connect(CONNECTION_STRING)
+        # Conectar ao banco de dados usando pymssql
+        conn = pymssql.connect(server=SERVER, user=USERNAME,
+                               password=PASSWORD, database=DATABASE)
         cursor = conn.cursor()
 
         # Executar a consulta na view vwcliente
-        cursor.execute("SELECT top 10 * FROM vwcliente")
+        cursor.execute("SELECT * FROM vwcliente")
         rows = cursor.fetchall()
 
         # Converter as linhas para uma lista de dicionários
         clientes = [
             dict(zip([column[0] for column in cursor.description], row)) for row in rows]
 
-        # Usar jsonable_encoder para serializar os dados
-        clientes_serializados = jsonable_encoder(clientes)
-
         # Retornar a resposta JSON
-        return JSONResponse(content=clientes_serializados)
+        return JSONResponse(content=jsonable_encoder(clientes))
 
     except Exception as e:
         raise HTTPException(
